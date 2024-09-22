883ed4bd37f1aaaca476805d2f85bcca9133e307
wisuwat <wisuwatpond01@gmail.com>
2024-09-22 16:39:13 +0700

    Try Request Respond

--- BackEnd/Express/index.js
+++ BackEnd/Express/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res) => {
+    console.log("Start Requesting");
+    res.send("<h1>This is a homepage</h1>")
+})
+
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 3000");
 })
