ade82985b1610b22cb8b856cb0f91a86cc24e731
wisuwat <wisuwatpond01@gmail.com>
2024-09-29 18:10:15 +0700

    Try req.body

--- BackEnd/GetPost/index.js
+++ BackEnd/GetPost/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/tacos', (req,res) => {
     res.send('Get /tacos')
 })
 
 app.post('/tacos', (req,res) => {
-    res.send('Post /tacos')
+    const { meat, qty } = req.body;
+    res.send(`Here is your ${qty} ${meat} taco(s)`)
 })
 
 app.listen(3000, () => {
