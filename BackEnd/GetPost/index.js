83fc4e4b22d8166695ab105ebd134fe57cfe642a
wisuwat <wisuwatpond01@gmail.com>
2024-09-29 18:51:43 +0700

    RESTful comments index

--- BackEnd/GetPost/index.js
+++ BackEnd/GetPost/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: "Todd",
+        comment: "lol, that is so funny"
+    },
+    {
+        username: "Skyler",
+        comment: "I want to go birdwatching with my dog"
+    },
+    {
+        username: "Shaniqua",
+        comment: "Please delete your account"
+    },
+    {
+        username: "Melissa",
+        comment: "Hey"
+    }
+]
+
+app.get('/comments', (req,res) => {
+    res.render('comments/index', { comments });
+})
 
 app.get('/tacos', (req,res) => {
     res.send('Get /tacos')
