fe6d61c74ad89202be22091a98a46052866cb29e
wisuwat <wisuwatpond01@gmail.com>
2024-10-03 12:12:19 +0700

    Mongoose Update & findAndUpdate

--- Database/Mongoose/index.js
+++ Database/Mongoose/index.js
@@ -26,16 +26,18 @@ const movieSchema = new Schema({
 const Movie = mongoose.model('Movie', movieSchema)
 // const amadeus = new Movie({title: "Amadeus", year: 1986, score: 9.5, rating: "R"});
 
-Movie.insertMany([
-  { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-  { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-  { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-  { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-  { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' } 
-])
-  .then(data => {
-    console.log("IT WORKED")
-    console.log(data)
-  })
+// Movie.insertMany([
+//   { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//   { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//   { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//   { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//   { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' } 
+// ])
+//   .then(data => {
+//     console.log("IT WORKED")
+//     console.log(data)
+//   })
 
 //Movie.find({}).then(data => console.log(data))
+//Movie.updateMany({title: {$in: ['Amadeus', 'Stand By me']}}, {score: 10}).then(res => console.log(res))
+//Movie.findOneAndUpdate({title: 'The Iron Giant'}, {score: 7.8}, {new: true}).then(m => console.log(m))
