cec5558c51dd269562cfcb8e3be62edccfba0ecd
wisuwat <wisuwatpond01@gmail.com>
2024-10-08 18:07:12 +0700

    E + M Create Product Detail Page

--- Database/MongooseExpress/index.js
+++ Database/MongooseExpress/index.js
@@ -20,6 +20,12 @@ app.get('/products', async (req,res) => {
     res.render('products/index', { products })
 })
 
+app.get('/products/:id', async (req,res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product })
+})
+
 app.listen(3000, () => {
     console.log("APP IS RUNNING ON PORT 3000")
 })
