176c4f47037e4f4b7ead9e3d3443e34592eb326f
wisuwat <wisuwatpond01@gmail.com>
2024-10-08 17:19:42 +0700

    E + M Create Model

--- Database/MongooseExpress/index.js
+++ Database/MongooseExpress/index.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product')
+
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/farmStand');
   console.log('Connected to MongoDB');
 }
 
