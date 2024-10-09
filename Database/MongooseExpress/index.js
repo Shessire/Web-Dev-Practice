b1c9683066b5ff36c05cbcb936614ba6ac2f9a59
wisuwat <wisuwatpond01@gmail.com>
2024-10-09 14:46:34 +0700

    E + M Delete Product

--- Database/MongooseExpress/index.js
+++ Database/MongooseExpress/index.js
@@ -55,6 +55,12 @@ app.put('/products/:id', async (req,res) => {
     res.redirect(`/products/${product._id}`)
 })
 
+app.delete('/products/:id', async (req,res) => {
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+})
+
 app.listen(3000, () => {
     console.log("APP IS RUNNING ON PORT 3000")
 })
