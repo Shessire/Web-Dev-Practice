1e59475907273e780b1b3a4397c85d9bed21fce5
wisuwat <wisuwatpond01@gmail.com>
2024-10-13 17:11:57 +0700

    Try More on Middleware

--- Database/Middleware/index.js
+++ Database/Middleware/index.js
@@ -5,20 +5,18 @@ const morgan = require('morgan');
 app.use(morgan('dev'))
 
 app.use((req,res,next) => {
-    console.log("First")
-    return next()
-})
-
-app.use((req,res,next) => {
-    console.log('Second')
-    return next();
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next()
 })
 
 app.get('/', (req,res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send('Home Page')
 })
 
 app.get('/dogs', (req,res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send('Woof')
 })
 
