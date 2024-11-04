242bdb44a0f8ec15e923ae830435d0e1ba4de75b
wisuwat <wisuwatpond01@gmail.com>
2024-11-04 16:30:31 +0700

    Learn One To Few

---
+++ Database/MongoRelationship/Models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationshipDemo');
+  console.log('Connected to MongoDB');
+}
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+makeUser();
