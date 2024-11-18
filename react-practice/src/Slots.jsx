a39f36258c369be935bdf3be92f5f7653dbdca95
wisuwat <wisuwatpond01@gmail.com>
2024-11-18 16:47:28 +0700

    Refactor: Slot Machine

--- react-practice/src/Slots.jsx
+++ react-practice/src/Slots.jsx
@@ -2,20 +2,16 @@ function Slots ({val1, val2, val3}) {
     const allValues = [val1, val2, val3]
     const isWinner = val1 === val2 && val2 === val3;
     const styles = {color: isWinner ? "green" : "red"}
-    const resultMessage = isWinner ? (
-        <div>
-            <h2 style={styles}>You win!</h2>
-            <h3>Congrats!</h3>
-        </div>
-    ) : (
-        <h2 style={styles}>You lose :(</h2>
-    )
+    
     return (
         <>
             {allValues.map((v) => (
                 <h1 style={{display: "inline-block"}}>{v}</h1>
             ))}
-            {resultMessage}
+            <h2 style={styles}>
+                {isWinner ? "You win!" : "You lose :("}
+            </h2>
+            {isWinner && <h3>Congrats!</h3>}
         </>
     )
 }
