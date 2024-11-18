8c3d73bfce4571adb505f43d92ebb9b1626fd9f2
wisuwat <wisuwatpond01@gmail.com>
2024-11-18 15:03:46 +0700

    React Conditionals

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import Greeter from './Greeter'
 import Die2 from './Die2'
 import ListPicker from './ListPicker'
+import DoubleDice from './DoubleDice'
 
 function App() {
   
   return (
     <>
-      <Greeter person="Bill"/>
+      {/* <Greeter person="Bill"/>
       <Die2 numSides={20}/>
       <Die2/>
       <Die2 numSides={10}/>
-      <ListPicker values={[1,2,3]}/>
+      <ListPicker values={[1,2,3]}/> */}
+      <DoubleDice/>
     </>
   )
 }
