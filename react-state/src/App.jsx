0f259b191354e5bf9a4e14195bf0f9b38754e788
wisuwat <wisuwatpond01@gmail.com>
2024-12-11 16:16:29 +0700

    Pass Fn as Props

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -5,6 +5,15 @@ import ScoreKeeper from './ScoreKeeper'
 import EmojiClicker from './EmojiClicker'
 import ScoreKeeper2 from './ScoreKeeper2'
 import LuckyN from './LuckyN'
+import { sum } from './utils'
+
+function lessThan4 (dice) {
+  return sum(dice) < 4
+}
+
+function allSameValue (dice) {
+  return dice.every((v) => v === dice[0])
+}
 
 function App() {
 
@@ -15,8 +24,8 @@ function App() {
       {/* <ScoreKeeper /> */}
       {/* <EmojiClicker /> */}
       {/* <ScoreKeeper2 numPlayer={10} target={3} /> */}
-      <LuckyN />
-      <LuckyN numDice={3} goal={11} />
+      <LuckyN winCheck={lessThan4} title='Roll less than 4' />
+      <LuckyN winCheck={allSameValue} title='Roll the same number' />
     </>
   )
 }
--- react-state/src/LuckyN.jsx
+++ react-state/src/LuckyN.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { getRolls, sum } from "./utils";
+import { getRolls } from "./utils";
 import Dice from "./Dice";
 
-function LuckyN ({ numDice = 2, goal = 7}) {
+function LuckyN ({ title = "Dice Game", numDice = 2, winCheck }) {
     const [dice, setDice] = useState(getRolls(numDice))
 
-    const isWinner = sum(dice) === goal;
+    const isWinner = winCheck(dice);
 
     const roll = () => setDice(getRolls(numDice));
 
     return (
         <div className="LuckyN">
-            <h1>Lucky{goal} {isWinner && "You Win!"}</h1>
+            <h1>{title} {isWinner && "You Win!"}</h1>
             <Dice dice={dice} />
             <button onClick={roll}>Re-roll dice</button>
         </div>
