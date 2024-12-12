0694d0d98b5cf8d33470e328beb7bfbb3377dd51
wisuwat <wisuwatpond01@gmail.com>
2024-12-12 10:26:10 +0700

    Pass Fn to Update State

---
+++ react-state/src/Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.css"
+
+function Button ({ clickFunc, label = "Click Me" }) {
+    return (
+        <button className="Button" onClick={clickFunc}>
+            {label}
+        </button>
+    )
+}
+
+export default Button;
--- react-state/src/LuckyN.jsx
+++ react-state/src/LuckyN.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { getRolls } from "./utils";
 import Dice from "./Dice";
+import Button from "./Button";
 
 function LuckyN ({ title = "Dice Game", numDice = 2, winCheck }) {
     const [dice, setDice] = useState(getRolls(numDice))
@@ -13,7 +14,8 @@ function LuckyN ({ title = "Dice Game", numDice = 2, winCheck }) {
         <div className="LuckyN">
             <h1>{title} {isWinner && "You Win!"}</h1>
             <Dice dice={dice} />
-            <button onClick={roll}>Re-roll dice</button>
+            {/* <button onClick={roll}>Re-roll dice</button> */}
+            <Button clickFunc={roll} label="Re-Roll"/>
         </div>
     )
 }
