e8d9b51d832b2d58fe60454398e7b695efe67569
wisuwat <wisuwatpond01@gmail.com>
2024-12-16 19:21:47 +0700

    Add a Loader

--- react-effects/src/App.jsx
+++ react-effects/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Counter from './Counter'
-import QuoteFetcher from './QuoteFetcher'
+import QuoteFetcherLoader from './QuoteFetcherLoader'
 
 function App() {
 
   return (
     <>
       {/* <Counter /> */}
-      <QuoteFetcher />
+      <QuoteFetcherLoader />
     </>
   )
 }
