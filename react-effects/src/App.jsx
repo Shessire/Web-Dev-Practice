312688a350c13f2c8643e4184a89cf398bc03d8b
wisuwat <wisuwatpond01@gmail.com>
2024-12-17 19:34:31 +0700

    Try Material UI

--- react-effects/src/App.jsx
+++ react-effects/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Counter from './Counter'
-import QuoteFetcherLoader from './QuoteFetcherLoader'
+import Button from '@mui/material/Button';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 function App() {
 
   return (
     <>
       {/* <Counter /> */}
-      <QuoteFetcherLoader />
+      <Button color='success' variant="contained">Contained</Button>
+      <Button size='small' variant="text" onClick={() => alert("HI!!")}>Text</Button>
+      <Button variant="outlined">Outlined</Button>
+      <AcUnitIcon />
     </>
   )
 }
