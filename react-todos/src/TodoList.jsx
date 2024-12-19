079552a564ae6b901e5b5ef25256b3a6b2e86c45
wisuwat <wisuwatpond01@gmail.com>
2024-12-19 13:25:34 +0700

    Add LocalStorage

--- react-todos/src/TodoList.jsx
+++ react-todos/src/TodoList.jsx
@@ -1,18 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import List from '@mui/material/List';
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 
-const initialTodos = [
-    { id: 1, text: "walk the dog", completed: false },
-    { id: 2, text: "walk the cat", completed: false },
-    { id: 3, text: "walk the fish", completed: true },
-    { id: 4, text: "walk the chickens", completed: false },
-]
+// const initialTodos = [
+//     { id: 1, text: "walk the dog", completed: false },
+//     { id: 2, text: "walk the cat", completed: false },
+//     { id: 3, text: "walk the fish", completed: true },
+//     { id: 4, text: "walk the chickens", completed: false },
+// ]
+
+const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+    if (!data) {
+        return []
+    } else {
+        return data;
+    }
+}
 
 function TodoList() {
 
-    const [todos, setTodos] = useState(initialTodos)
+    const [todos, setTodos] = useState(getInitialData)
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos]);
 
     const removeTodo = (id) => {
         setTodos((prevTodos) => {
